6e5395afb68c325caf597b205af0d6eaef7be376
JOUGLA Benoit <benoit.jougla@infotel.com>
2023-07-27 15:45:58 +0200

    Add gitignore

--- src/main/java/aqw42/App.java
+++ src/main/java/aqw42/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static String get_message()
     {
-        return "Hello World !";
+        return "Hello World 2 !";
     }
 
     public static void main( String[] args )
